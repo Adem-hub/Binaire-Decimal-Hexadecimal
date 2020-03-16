8f362585d8bd370e0d891cf0379f0d8ea35b56d0
Adem-hub <57374875+Adem-hub@users.noreply.github.com>
2020-03-16 17:56:12 +0100

    Add files via upload

--- Binaire.py
+++ Binaire.py
@@ -1,18 +1,61 @@
 import copy
 from copy import deepcopy
-"Entrez un string contenant le binaire"
-def BinaryToDecimal(binary):
-    binary=deepcopy(binary)
+"Entrez un argument qui est un string contenant le binaire, le decimal ou bien l'hexadecimal"
+#Le binaire est fini.
+#Decimal ....
+#HexaDecimal ....
+
+def BinaireToAll(binaire):
+    Dec= BinaireToDecimal(binaire)
+    Hex=DecimalToHexa(Dec)
+    Tot= "Decimal: "+ str(Dec)+ " \nHexaDecimal: "+ str(Hex)
+    print(Tot+'\n')
+    return [Dec,Hex]
+
+
+
+def BinaireToDecimal(binaire):
+    binaire=deepcopy(binaire)
     Decimal=[]
     facteur=1
-    a= reversed(binary)
+    a= reversed(binaire)
     for item in a:
-        it=int(item)
-        it= it*facteur
-        Decimal.append(it)
-        facteur*=2
+        if item !='1' and item!= '0':
+            return "Ce n'est pas du binaire"
+        else:
+            it=int(item)
+            it= it*facteur
+            Decimal.append(it)
+            facteur*=2
     Total=sum(Decimal)
     return Total
+
+def DecimalToHexa(binaire):
+    binaire= deepcopy(binaire)
+    hexa=""
+    a=int(binaire)%16
+    while binaire!=0:
+        if a==10:
+            hexa= hexa+ "A"
+        if a==11:
+            hexa= hexa+ "B"
+        if a==12:
+            hexa= hexa+ "C"
+        if a==13:
+            hexa= hexa+ "D"
+        if a==14:
+            hexa= hexa+ "E"
+        if a==15:
+            hexa= hexa+ "F"
+        else:
+            hexa= hexa+ str(a)
+        binaire=int(binaire)/16
+        a=int(binaire)%16
+    hexa= hexa.replace('0','')
+    hexa= hexa[::-1]
+    return hexa
+
+
 
 
 
